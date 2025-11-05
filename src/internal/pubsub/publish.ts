4732445941e1e36c4c674609c454a0592da155ca
Marcos Pablo <marquin.pablo@gmail.com>
2025-11-05 12:50:23 -0300

    feat(pubsub): add msgpack publish support

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import type { ConfirmChannel } from 'amqplib';
+import { encode } from '@msgpack/msgpack';
 
 export async function publishJSON<T>(
   ch: ConfirmChannel,
@@ -6,16 +7,38 @@ export async function publishJSON<T>(
   routingKey: string,
   value: T,
 ): Promise<void> {
-  const json = JSON.stringify(value);
+  const stringifiedJson = JSON.stringify(value);
 
-  if (json === undefined) {
-    throw new Error('Value is not JSON-serializabe');
+  if (stringifiedJson === undefined) {
+    throw new Error('Value is not JSON-serializable');
   }
 
-  const content = Buffer.from(json, 'utf8');
+  const content = Buffer.from(stringifiedJson, 'utf8');
 
   return new Promise((resolve, reject) => {
     ch.publish(exchange, routingKey, content, { contentType: 'application/json' }, (err) => {
+      if (err !== null) {
+        reject(new Error('Message was NACKed by the broker'));
+      }
+      resolve();
+    });
+  });
+}
+
+export async function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const content = Buffer.from(encode(value));
+
+  if (!content) {
+    throw new Error('Value is not messagepack-serializable');
+  }
+
+  return new Promise((resolve, reject) => {
+    ch.publish(exchange, routingKey, content, { contentType: 'application/x-msgpack' }, (err) => {
       if (err !== null) {
         reject(new Error('Message was NACKed by the broker'));
       }
